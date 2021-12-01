ec10906dbd0fc384abf3dc7822b7d6f69dac63a7
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2021-12-02 00:15:41 +0200

    java

--- ecourts_java/test.java
+++ ecourts_java/test.java
@@ -1,13 +1,18 @@
 package ecourts_java;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        
         Slot slot = new Slot();
-        List<Slot> slot_list = slot.getSlots(1, null);
+        List<Slot> slot_list = slot.getSlots(0, "",0,4);
         System.out.print(slot_list.size());
-
+        
+        
 
     }
     
